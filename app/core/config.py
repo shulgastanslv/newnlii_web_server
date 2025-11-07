78810da0da933f8250eaf4ed931a2730b53b363d
shulgastanslv <shulgastanslv@gmail.com>
2025-11-07 22:26:18 +0300

    feat: cors fixed

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-
+    
     class Config:
         env_file = ".env"
 
--- app/main.py
+++ app/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from app.db.session import engine
 from app.db.base import Base
 from app.api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
